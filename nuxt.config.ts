138ca6bedf48af2c27aa8660b2159e1e4eb92fb8
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-04-27 21:07:27 +0100

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,18 +4,19 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/css/tailwind.css"],
+
   devServer: {
     // host: "0.0.0.0", // 👈 bind inside container
     port: 3000,
   },
+
   runtimeConfig: {
     public: {
-      // auth0Domain: "YOUR_AUTH0_DOMAIN",
-      // auth0ClientId: "YOUR_AUTH0_CLIENT_ID",
       apiBase: process.env.API_URL || "http://localhost:8080",
       auth0Domain: process.env.AUTH0_DOMAIN,
       auth0ClientId: process.env.AUTH0_CLIENT_ID,
       auth0Audience: process.env.AUTH0_AUDIENCE,
     },
+    auth0ClientSecret: process.env.AUTH0_CLIENT_SECRET, // private by default
   },
 });
