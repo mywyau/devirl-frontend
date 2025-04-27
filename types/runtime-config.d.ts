9416f4cb9dc8b9c059050a1bc6c01d1e53ef10de
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-04-27 22:07:40 +0100

    try to fix configs

--- types/runtime-config.d.ts
+++ types/runtime-config.d.ts
@@ -1,12 +1,16 @@
+// types/runtime-config.d.ts
 export {}
 
 declare module 'nuxt/schema' {
   interface RuntimeConfig {
-    // Server-only keys
+    // private, server-only
+    auth0ClientSecret: string
   }
 
   interface PublicRuntimeConfig {
+    // public, exposed to client
     auth0Domain: string
     auth0ClientId: string
+    apiBase: string
   }
 }
