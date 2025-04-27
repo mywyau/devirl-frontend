a861d54573a4f9cb5c4ec135a4156813f1c41d9a
Michael <mywyaudev@gmail.com>
2025-04-27 21:10:00 +0100

    irl-7 Remove extra script tag (#7)
    * remove extra script tag
    
    * refactor dashbaord
    
    ---------
    
    Co-authored-by: michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>

--- layouts/quests-dashboard.vue
+++ layouts/quests-dashboard.vue
@@ -1,78 +1,73 @@
 <script setup lang="ts">
+// import ConsentPopup from "~/components/ConsentPopup.vue";
 
 definePageMeta({
-  middleware: 'auth',
-})
+  middleware: "auth",
+});
 
+import { useAuth } from "@/composables/useAuth";
+
+import type { User } from "@auth0/auth0-spa-js";
+import { onMounted, ref } from "vue";
+
+const auth = useAuth();
+const user = ref<User | null>(null);
+const isLoggedIn = ref(false);
+
+onMounted(async () => {
+  isLoggedIn.value = await auth.isAuthenticated();
+  if (isLoggedIn.value) {
+    user.value = await auth.getUser();
+    console.log("Logged in user:", user.value);
+  }
+});
 </script>
 
 <template>
-    <div
-      class="bg-gradient-to-br from-gray-900 via-gray-800 to-black min-h-screen text-gray-100 font-sans"
+  <div
+    class="bg-gradient-to-br from-gray-900 via-gray-800 to-black min-h-screen text-gray-100 font-sans"
+  >
+    <header
+      class="px-6 py-4 border-b border-gray-700 flex justify-between items-center"
     >
-      <header
-        class="px-6 py-4 border-b border-gray-700 flex justify-between items-center"
-      >
-        <NuxtLink to="/" class="text-xl font-bold text-white hover:text-gray-300">
-          Dev Irl
+      <NuxtLink to="/" class="text-xl font-bold text-white hover:text-gray-300">
+        Dev Irl
+      </NuxtLink>
+
+      <nav class="space-x-6 flex items-center">
+        <template v-if="isLoggedIn">
+          <span class="text-sm text-white/70">Hi, {{ user?.name }}</span>
+        </template>
+
+        <NuxtLink to="/quests" class="hover:text-gray-300">Quests</NuxtLink>
+        <NuxtLink to="/quests/new" class="hover:text-gray-300"
+          >Post Quest
         </NuxtLink>
-  
-        <nav class="space-x-6 flex items-center">
-          <template v-if="isLoggedIn">
-            <span class="text-sm text-white/70">Hi, {{ user?.name }}</span>
-          </template>
-  
-          <NuxtLink to="/quests" class="hover:text-gray-300">Quests</NuxtLink>
-          <NuxtLink to="/quests/new" class="hover:text-gray-300"
-            >Post Quest
+
+        <template v-if="isLoggedIn">
+          <NuxtLink to="/quest-dashboard" class="hover:text-gray-300">
+            Quest Dashboard
           </NuxtLink>
-  
-          <template v-if="isLoggedIn">
-            <NuxtLink to="/quest-dashboard" class="hover:text-gray-300">
-              Quest Dashboard
-            </NuxtLink>
-          </template>
-  
-          <template v-if="isLoggedIn">
-            <button @click="auth.logout()" class="hover:text-red-400 text-base">
-              Logout
-            </button>
-          </template>
-          <template v-else>
-            <button @click="auth.login()" class="hover:text-cyan-400 text-base">
-              Login
-            </button>
-          </template>
-        </nav>
-      </header>
-  
-      <main class="p-4">
-        <slot />
-        <!-- <ConsentPopup /> -->
-      </main>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  // import ConsentPopup from "~/components/ConsentPopup.vue";
-  
-  import { useAuth } from "@/composables/useAuth";
-  
-  import type { User } from "@auth0/auth0-spa-js";
-  import { onMounted, ref } from "vue";
-  
-  const auth = useAuth();
-  const user = ref<User | null>(null);
-  const isLoggedIn = ref(false);
-  
-  onMounted(async () => {
-    isLoggedIn.value = await auth.isAuthenticated();
-    if (isLoggedIn.value) {
-      user.value = await auth.getUser();
-      console.log("Logged in user:", user.value);
-    }
-  });
-  </script>
-  
-  <style scoped></style>
-  
+        </template>
+
+        <template v-if="isLoggedIn">
+          <button @click="auth.logout()" class="hover:text-red-400 text-base">
+            Logout
+          </button>
+        </template>
+        <template v-else>
+          <button @click="auth.login()" class="hover:text-cyan-400 text-base">
+            Login
+          </button>
+        </template>
+      </nav>
+    </header>
+
+    <main class="p-4">
+      <slot />
+      <!-- <ConsentPopup /> -->
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
