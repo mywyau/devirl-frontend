57d746f69a26921aae22212c06d320bf2d75537a
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-05-05 01:28:57 +0100

    fix nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: "node", 
+    preset: "node-server", 
   },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
