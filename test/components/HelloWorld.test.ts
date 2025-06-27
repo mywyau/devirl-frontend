840292b655206ac5296dc8bbf7da94dc8984f3cd
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-06-27 18:54:58 +0100

    adding schema tests

--- test/components/HelloWorld.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-// import MyComponent from "../MyComponent.vue";
-
-describe("MyComponent", () => {
-  it("renders properly", () => {
-    const hello = "Hello";
-    expect(hello).toContain("Hello");
-  });
-});
