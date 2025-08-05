21c47861c5f89bca9a064b9573b0051a67c6a186
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-08-05 13:33:22 +0100

    dev-54 fixing issues with estimations, bids and hiscore levels

--- components/ui/hiscores/TotalDevCountMessage.vue
+++ components/ui/hiscores/TotalDevCountMessage.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 const props = defineProps<{
+    id: string;
     hasLoaded: boolean;
     totalItems: number;
 }>();
 </script>
 
 <template>
-    <p v-if="hasLoaded" class="text-center w-full text-white mb-10">
+    <p :id="id" v-if="hasLoaded" class="text-center w-full text-white mb-10">
         There {{ totalItems === 1 ? 'is' : 'are' }}
         <span class="text-teal-300 font-bold">{{ totalItems }}</span>
         developer{{ totalItems === 1 ? '' : 's' }}
